97f6e4e1d60b974503248fa4fee013c74428cc7e
Joey Andres <jandres@ualberta.ca>
2016-12-09 21:44:56 -0700

    test env

--- test/env/linux/common/dependencies.sh
+++ test/env/linux/common/dependencies.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-# Stick with apt-get for now until further notice.
-sudo apt-get install -y g++ cmake
+# Install GNU MultiPrecision (GMP).
+wget https://gmplib.org/download/gmp/gmp-6.1.1.tar.lz -P /tmp
+cd /tmp && \
+  ls -la && \
+                  gmp-6.1.1.tar.lz
+  tar --lzip -xvf gmp-6.1.1.tar.lz && \
+  cd gmp-6.1.1 && \
+  ./configure --enable-cxx && \
+  make && \
+  sudo make install
